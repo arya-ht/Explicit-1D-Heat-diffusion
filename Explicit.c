66230322ed81662ae16f14af73527fbbe7372410
arya-ht <aryaht23@gmail.com>
2017-07-29 22:50:53 -0500

    formatting

--- Explicit.c
+++ Explicit.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -14,108 +13,96 @@
 
 int xstep = xMAX / dx + 1;
 int tstep = (double)tMAX / dt + 1;
-char fileName[100] = "Exp1D.csv";
-char fileNameBlockCSV[100];
-char fileNameBlockActualCSV[100];
-char errorFile[101];
 
 double F(double, double);
 void U0(double u[xstep][tstep]);
 void Boundary(double u[tstep][xstep], int tRow);
 void fill(double[tstep][xstep], int);
-void print(double u[tstep][xstep]);
+void print(double u[tstep][xstep] );
 double exact(double, double);
+void Explicit();
 
-int main(void)
-{
-	double U[tstep][xstep];
-
-	U0(U);
-	int i;
-	for (i = 1; i<tstep; i++)
-	{
-		Boundary(U, i);
-		fill(U, i);
-	}
-
-	print(U);
-	if (lam >= 0.5)
-	{
-		printf("\n\ algorithm is unstable because Lambda is = %1.5lf", lam);
-	}
-
+int main(void){
+	Explicit();
 	return 0;
 }
-void print(double u[tstep][xstep])
-{
+void print(double u[tstep][xstep]){
 	int i, j;
-
-
-	FILE *error = fopen("error.csv", "w");
-	FILE *estimate = fopen(fileName, "w");
-	FILE *actual = fopen("actual.csv", "w");
-
-	for (i = 0; i<tstep; i++)
-	{
-
-		for (j = 0; j<xstep; j++)
-		{
-			printf("\t%lf", u[i][j]);
-			fprintf(estimate, "%lf,", u[i][j]);
+	FILE *error = fopen("error.csv", "w"), *estimate = fopen("Exp1D.csv", "w"),
+	 *actual = fopen("actual.csv", "w"),*t = fopen("t.csv","w"), *x = fopen("x.csv","w");
+    double error_n = 0.0;
+	for (i = 0; i<tstep; i++){
+    printf("Estimate: \n");
+		for (j = 0; j<xstep; j++){
+            fprintf(x,"%d",j);      ///x axis
+			printf("%lf, ", u[i][j]);
+			fprintf(estimate, "%lf ,", u[i][j]);
 		}
 		fprintf(estimate, "\n");
-
-		for (j = 0; j<xstep; j++)
-		{
-			printf("\t%lf", exact(i*dt, j*dx));
+       printf("\nExact: \n");
+		for (j = 0; j<xstep; j++){
+			printf("%lf, ", exact(i*dt, j*dx));
 			fprintf(actual, "%lf,", exact(i*dt, j*dx));
 		}
 		fprintf(actual, "\n");
+		        printf("\nError: \n");
 
-		for (j = 0; j<xstep; j++)
-		{
-			printf("\t%lf", (exact(i*dt, j*dx) - u[i][j]) / exact(i*dt, j*dx) * 100);
+		for (j = 0; j<xstep; j++)	{
+
+        error_n = fabs((exact(i*dt, j*dx) - u[i][j]) / exact(i*dt, j*dx)) * 100;
+
+        if (j == xstep - 1 || j == 0){ // because error becomes infinity (0/0).
+                error_n = 0.0;
+        }
+			printf("%lf%% ",error_n);
 			if (j != 0 && j != xstep - 1)
-				fprintf(error, "%lf,", (exact(i*dt, 1 * dx) - u[i][1]) / exact(i*dt, 1 * dx) * 100);
+				fprintf(error, "%lf,", fabs((exact(i*dt, 1 * dx) - u[i][1]) / exact(i*dt, 1 * dx)) * 100);
 		}
 		fprintf(error, "\n");
-
-		printf("\n\n\n\n");
+        fprintf(t,"%d",i); ///y axis
+		printf("\n===================================================================================================================\n");
 	}
 
 	fclose(error);
 	fclose(actual);
 	fclose(estimate);
+	fclose(t);
+	fclose(x);
 
 }
-
-
-void U0(double u[tstep][xstep])
-{
+void U0(double u[tstep][xstep]){
 	int i;
-	for (i = 0; i<xstep; i++)
-	{
+	for (i = 0; i<xstep; i++){
 		u[0][i] = sin(i*dx*n);
 	}
 }
-void Boundary(double u[tstep][xstep], int tRow)
-{
+void Boundary(double u[tstep][xstep], int tRow){
 	u[tRow][0] = 0;
 	u[tRow][xstep - 1] = 0;
 }
-void fill(double u[tstep][xstep], int tRow)
-{
+void fill(double u[tstep][xstep], int tRow){
 	int i;
-	for (i = 1; i<xstep - 1; i++)
-	{
+	for (i = 1; i<xstep - 1; i++){
 		u[tRow][i] = u[tRow - 1][i] + lam*(u[tRow - 1][i - 1] - 2 * u[tRow - 1][i] + u[tRow - 1][i + 1]) + dt*F(i*dt, i*dx);
 	}
 }
-double exact(double t, double x)
-{
+double exact(double t, double x){
 	return exp(-n*n*t)*sin(n*x);
 }
-double F(double t, double x)
-{
+double F(double t, double x){
 	return 0;
+}
+void Explicit(){double U[tstep][xstep];
+
+	U0(U);
+	int i;
+	for (i = 1; i<tstep; i++){
+		Boundary(U, i);
+		fill(U, i);
+	}
+
+	print(U);
+	if (lam >= 0.5){
+		printf("\n\a Explicit is unstable because Lambda is = %1.5lf", lam); //alert with a beep!
+	}
 }
